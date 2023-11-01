cb1e71103bd95ff3169da5655fa17cc58f272e51
Tadon <Mark.Klocek@GMail.com>
2023-11-01 14:12:50 -0700

    Lots of updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # YellowPages
  Website Scraper for Yellow Pages
+
+ Copyright @ Mark Klocek 2023
--- info.py
+++ info.py
@@ -3,49 +3,110 @@ from bs4 import BeautifulSoup
 import csv
 import time
 filename = 'csv.csv'
-counter = 0
-for i in range(1, 101):
+business_counter = 0
+from categories import category_list
+from Cities_and_states import Cities_and_states
+#iterating through cities in cali
+existing_numbers = {}
+with open(filename, 'r',encoding='utf-8') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        existing_numbers[row[3]] = ''
+        business_counter += 1
+for cities in Cities_and_states.California:
+    #iterating through categories in the city
     
+    for categorie in category_list:
+        #iterating through pages of the categories in each city
+        counter = 1
+        while True:
+            time.sleep(3)
+            url = f'https://www.yellowpages.com/search?search_terms={categorie}&geo_location_terms={cities}&page={counter}'
 
-    url = f'https://www.yellowpages.com/search?search_terms=plumbers&geo_location_terms=ca&page={i}'
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0'
+            }
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0'
-    }
+            r = requests.get(url, headers=headers)
 
-    r = requests.get(url, headers=headers)
+            mark_soup = BeautifulSoup(r.content, 'html.parser')
+            
 
-    mark_soup = BeautifulSoup(r.content, 'html.parser')
-    
+            mark_html = mark_soup.find_all('div', class_ = 'v-card')
 
-    mark_html = mark_soup.find_all('div', class_ = 'v-card')
+            
+            
+            for item in mark_html:
+                
+                
+                name = item.find('a', class_ = 'business-name').text
+                
+                    
+                try:
+                    number = item.find('div', class_= 'phones phone primary').text
+                except:
+                    number = '_No number provided_'
 
-    
-    
-    for item in mark_html:
-        counter += 1
-        
-        name = item.find('a', class_ = 'business-name').text
-        try:
-            number = item.find('div', class_= 'phones phone primary').text
-        except:
-            number = 'No number provided'
-        
-        try:
-            locality = item.find('div', class_='locality').text
-        except:
-            locality = 'No adress provided'
+                try:
+                    street_address = item.find('div', class_='street-address').text
+                except:
+                    street_address = '_No address provided_'
 
-        try:
-            categories = item.find('div', class_= 'categories').text
-        except:
-            categories = 'No category listed'
+                try:
+                    locality = item.find('div', class_='locality').text
+                except:
+                    locality = '_No locality provided_'
+                
+                try:
+                    categories = item.find('div', class_= 'categories').text
+                except:
+                    categories = '_No category listed_'
+                
+                link_element = item.find('a', class_= 'track-more-info')
+                if link_element and link_element.has_attr('href'):
+                    link = link_element['href']
+                else:
+                    link = '_More info not found_'
+                
 
-        with open(filename, 'a', newline = '') as file:
-            write_row = csv.writer(file)
-            write_row.writerow([name, locality, number, categories])
-        
-        
+                email = '_No Email Provided_'
 
-    
-    print(counter)
+                if (number not in existing_numbers) and number != '_No number provided_':
+                    
+
+                    if link != '_More info not found_':
+                        new_url = f'https://www.yellowpages.com{link}'
+                        nr = requests.get(new_url, headers=headers)
+                        new_soup = BeautifulSoup(nr.content, 'html.parser')
+                        email_link = new_soup.find('a', class_='email-business')
+
+                        if email_link:
+                            try:
+                                email = email_link['href'].split('mailto:')[-1]
+                            except:
+                                pass
+                        else:
+                            pass
+                            
+                    else:
+                        pass
+                    
+                        
+
+                    with open(filename, 'a', newline = '', encoding = 'utf-8') as file:
+                        write_row = csv.writer(file)
+                        write_row.writerow([name, street_address, locality, number, email, categories])
+                    business_counter += 1
+
+                existing_numbers[number] = ''
+
+            print(f'Processed page {counter} of {categorie} in {cities}, with a total of {business_counter} businesses added.')
+
+            next_page_exists = mark_soup.find('a', class_= 'next ajax-page')
+            if next_page_exists:
+                counter += 1
+            else:
+                break
+
+print('Scraping complete!')
